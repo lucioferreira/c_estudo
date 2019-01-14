9881ec607077b875dfd92e10e27e1d640c73e4f8
Lucio Ferreira <lucio.ferreira@gmail.com>
2019-01-14 18:41:54 -0400

    adicao de um programa de testes basico e os arquivos necessarios para acesso ao sqlite e o banco de dados inicial

---
+++ server/client_teste.c
@@ -0,0 +1,88 @@
+/*
+    C ECHO client example using sockets
+*/
+#include<stdio.h> //printf
+#include<stdlib.h>
+#include <unistd.h>
+#include<string.h>    //strlen
+#include<sys/socket.h>    //socket
+#include<arpa/inet.h> //inet_addr
+
+int main(int argc , char *argv[])
+{
+    int sock;
+    struct sockaddr_in server;
+    char server_reply[2000];
+    int opcao = -1;
+    char json[100];
+    
+    memset(&json, 0, sizeof(json) );
+
+    //Create socket
+    sock = socket(AF_INET , SOCK_STREAM , 0);
+    if (sock == -1) {
+        printf("erro ao criar o socket");
+    }
+    puts("Socket criado");
+
+    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server.sin_family = AF_INET;
+    server.sin_port = htons( 8888 );
+
+    //Connect to remote server
+    if (connect(sock , (struct sockaddr *)&server , sizeof(server)) < 0) {
+        perror("falha ao conectar. Erro");
+        return 1;
+    }
+
+    puts("Connected\n");
+    
+    printf("\n\n\n");
+ 	 printf("0 - sair\n");
+ 	 printf("1 - autenticar usuario\n");
+ 	 printf("2 - listar_mesas\n");
+ 	 printf("\n\n\n");
+    	
+    printf("entre com uma opção: ");
+	 scanf("%d", &opcao);
+	 switch(opcao) {
+		case 0:
+			close(sock);
+			return 0;			
+		case 1: /* autenticar ususario */
+			puts("autenticar_usuario");
+  			strcpy(json, "{\"cmd\":\"autenticar_usuario\", \"usuario\":\"lucio\", \"senha\":\"lucio\"}");
+			break;
+		case 2: /* listar_mesas */
+			puts("listar_mesas");
+			strcpy(json, "{\"id_usuario\":\"1\", \"cmd\":\"listar_mesas\"}");
+			break;
+		otherwise:
+			puts("comando inexistente");
+			close(sock);
+			return 1;
+		}    	
+
+  	 memset(&server_reply, 0, sizeof(server_reply) );
+  	 // char *message = "{\"comando\":\"get_mesas\"}";
+    printf("Enviando json: %s\n", json);
+
+    //envia o json
+    if( send(sock , json , strlen(json) , 0) < 0) {
+       puts("falha no envio");
+       return 1;
+    }
+
+    //Receive a reply from the server
+    if( recv(sock , server_reply , 2000 , 0) < 0) {
+       puts("recv falhou");
+    }
+
+    puts("resposta do Server: ");
+    puts(server_reply);
+    puts("---  fim  ---");
+
+    close(sock);
+ 
+    return 0;
+}
